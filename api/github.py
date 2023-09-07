6013971eef0aa3a6c7a1767c62c3edc88e4a481d
Pranav <pranavprajapati586@gmail.com>
2023-09-07 21:18:36 +0530

    feat:Github API downloads only .md files

--- api/github.py
+++ api/github.py
@@ -1,25 +1,32 @@
+import os
 import requests
-# Define the repository details
-owner=""
-repo = ""
-folder_path = ""
-# GitHub API endpoint URL
-url = f"https://api.github.com/repos/{owner}/{repo}/contents/{folder_path}"
 
-try:
-    # Send a GET request to the GitHub API
-    response = requests.get(url)
+def Github(url):
+    # GitHub API endpoint URL for the folder
+    url = url
 
-    # Check if the request was successful (HTTP status code 200)
-    if response.status_code == 200:
-        data = response.json()
+    try:
+        response = requests.get(url)
 
-        # Iterate through the folder contents and print their names
-        for item in data:
-            print(item["name"])
+        if response.status_code == 200:
+            data = response.json()
+            # Iterate through the folder contents and download each file
+            for item in data:
+                if item["type"] == "file" and item["name"].endswith(".md"):
+                    file_url = item["download_url"]
+                    file_name = os.path.join("../data", item["name"])
 
-    else:
-        print(f"Failed to fetch folder. Status code: {response.status_code}")
+                    # Download the file
+                    file_response = requests.get(file_url)
 
-except requests.exceptions.RequestException as e:
-    print(f"Request error: {e}")
+                    if file_response.status_code == 200:
+                        with open(file_name, 'wb') as f:
+                            f.write(file_response.content)
+                            print(f"Downloaded: {file_name}")
+                    else:
+                        print(f"Failed to download file: {file_name}")
+        else:
+            print(f"Failed to fetch folder. Status code: {response.status_code}")
+
+    except requests.exceptions.RequestException as e:
+        print(f"Request error: {e}")
